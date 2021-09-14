ff465f77ee7e8cfbe0034f0f2080e1cafc2ad55a
julesfrl <90465381+julesfrl@users.noreply.github.com>
2021-09-14 14:12:35 +0200

    Update page3.py

--- page3.py
+++ page3.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from keras.utils import np_utils
 from PIL import Image
 import tensorflow as tf
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 from asrtoolkit import cer
 from streamlit_drawable_canvas import st_canvas
 
