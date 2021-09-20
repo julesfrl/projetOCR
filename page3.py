a527083eb263978a0cd6d8700550f18278bf5ecd
julesfrl <90465381+julesfrl@users.noreply.github.com>
2021-09-20 16:51:24 +0200

    Update page3.py

--- page3.py
+++ page3.py
@@ -167,10 +167,10 @@ def page3():
 
             X_test = tf.cast(X_test, dtype = tf.float32)
             st.write("X_test", X_test)
-            st.write("np.expand_dims(X_test, -1)", np.expand_dims(X_test, -1))
+            st.write("np.expand_dims(X_test[:1], -1)", np.expand_dims(X_test[:1], -1))
             st.write(np.expand_dims(X_test, 0))
             #l = greedy_decoder(model(np.expand_dims(X_test, -1)))
-            l = greedy_decoder(model(np.expand_dims(X_test, 0)))
+            l = greedy_decoder(model(np.expand_dims(X_test[:1], -1)))
             st.write("Et voici les résultats de votre modèle :")
 
             st.write('**Prediction**')
